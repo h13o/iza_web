2241bb077c44da0be5cd1c7ff3d847c4b51e028d
Rintaro Hasegawa <rintaro.hasegawa@gmail.com>
2020-11-28 20:03:00 +0900

    delete unnecessary files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <footer >
       </footer>
     )
   }
